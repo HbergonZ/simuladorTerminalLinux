c52836b7f623b48dfb183002564832e81b3eef88
Hbergon <67556869+HbergonZ@users.noreply.github.com>
2023-11-06 17:14:35 -0400

    Atualização
    Simplificação do código, ajuste de bugs e adição de comentários.

--- Terminal2.py
+++ Terminal2.py
@@ -1,75 +1,65 @@
 import os
 
-comandos_utilizados = [] #criação do vetor para listar os comandos utilizados (válidos) [vou validar mais pra frente]
+comandos_utilizados = [] #criação de um vetor para armazenas os comandos válidos utilizados (no caso vai ser  histórico).
 
-print("Terminal de Linux")
+print("Terminal de Linux") #título do terminal (acho que não precisa)
 
-rodando = True
-command2 = ''
-while rodando: #O programa entra em um loop infinito, onde o usuário pode inserir comandos repetidamente.
-    command_err = False # a condição de erro no comando começa falsa pois sem comando, não tem como dar erro, certo?
+command2 = '' #command2 é uma variável que armazena um comando da lista dos utilizados ou o último comando. Por isso ela inicia nula.
+rodando = True #rodando é a variável que mantem o loop do programa, armazenando True até a utilização do comando exit().
 
-    if command2 == '':
+while rodando: #início do loop
+    if command2 == '': #inicialmente uma verificação se foi usado alguma chamada de um comando da lista de comando utilizados (inicialmente não tem nenhum).
         command = input("osh> ")
-    
     else:
-        command = command2
-        command2 = ''
-    
-    args = command.split() #O comando é separado em argumentos, já que pode ter mais de um. Esse argumentos serão armazenados no vetor args. Os argumentos serão lidos na ordem das palavras da String.
+        command = command2 #caso aja, o comando utilizado passa a ser o comando solicitado da lista.
+        command2 = '' #e o command2 volta a ser nula para que não fique infinitamente no mesmo comando.
 
-    if args[0] == "exit" and len(args) ==1:
+    args = command.split() #aqui há uma divisão da string para fazer a verificação do comando
+
+
+    #Abaixo eu optamos por verificar se o comando utilizado trata-se inicialmente de um comando so simulador e não do terminal, e só por fim, tenta executar o comando no terminal.
+    if command == "!!":  #caso o comando seja para executar o último comando utilizado da lista
+        if len(comandos_utilizados) > 0: #verifica se a lista possui algum comando já executado, caso sim, executa o último utilizado.
+            command2 = comandos_utilizados[len(comandos_utilizados) - 1] #aqui a variavel command2 recebe o último comando.
+        else:
+            print("Nenhum comando correspondente no histórico.") #caso nãõ haja pelo menos um comando na lista, o usuário é avisado.
+            
+    elif command.startswith("!"): #caso a condição de cima nãõ seja atentida, verifica se o comando inicia com "!"
+        try: #aqui tenta executar o comando ex !1 !2 !3...
+            num = int(command[1:]) #aqui tenta identificar o número após a exclamação
+            if num == 1 and comandos_utilizados:#se for 1, o comando utilizado é o primeiro da lista
+                command2 = comandos_utilizados[0]
+            elif 1 <= num <= len(comandos_utilizados): #se tiver entre 1 e o tamanho da lista o comando executa o comando da posição indicada.
+                command2 = comandos_utilizados[num - 1]
+            else:
+                print("Número de histórico inválido") #caso contrário retorna numéro inválido.
+        except ValueError:
+            print("Comando de histórico inválido") #caso de erro no comando, retorna comando inválido.
+
+    elif args[0] == "exit" and len(args) == 1: #caso o comando seja exit, exibe print finalizado e a variável do loop passa a ser False. Após isso, tem um continue que ignora o que vem após e volta para o início do loop para fazer a verificação do rodando.
         print("Loop finalizado.")
         rodando = False
         continue
 
+    elif args[0] == "history" and len(args) == 1: #caso o comando seja history, exibe a lista de comandos utilizados, sendo enumeradas reverso (de baixo pra cima) iniciando em 1.
+        for index, valor in enumerate(reversed(comandos_utilizados), start=1):
+            if valor != "history": #isso aqui é para não contabilizar o comando history na lista da comando utilizados, pois nãõ é um comando do terminal.
+                print(f"{len(comandos_utilizados) - index + 1}) {valor}")
 
-    elif args[0] == "history" and len(args) ==1: #Nesse caso, se o primeiro argumento (palavra) do comando inserido for history, automaticamente ele vai reconhecer como comando history dos últimos comandos válidos enviados. Se por um acaso o comando consta com mais de um argumento além do history, o comando não é reconhecido.
-            for index, valor in enumerate(reversed(comandos_utilizados), start=1):
-                if valor != "history":
-                    print(f"{len(comandos_utilizados) - index + 1} {valor}")
-    
-    if(command_err == False and command != 'history' and not command.startswith("!")):
-        pid = os.fork() #utilização do fork para criar um processo filho.
-
-    if pid == 0: #Nesse caso entramos dentro do contexto do processo filho, onde os comandos serão executados;
-
-        if not command.startswith("!") and args[0] != "history": #aqui é a validação dos comandos. Só entra pra validação caso não comece com '!' e o primeiro argumento do comando não seja history (que ia dar erro por ser um comando do simulador e não do terminal do Linux).
+    else:
+        pid = os.fork() #por fim, se nenhuma verificação acima for atendida, cria-se o processo filho para tentar executar o comando no terminal do linux.
+        if pid == 0: #Nesse caso entramos dentro do contexto do processo filho, onde os comandos serão executados;
             try:
-                os.execvp(args[0], args) #Aqui ele tenta executar o comando, se der certo, blz, segue o código.
-            except FileNotFoundError:
-                print(f"Comando não encontrado: {args[0]}") #Exceção para erro. Caso não encontre o comando, a variável erro passa a ser True prara que lá na linha de verificação dessa variável ele não faça adicione o comando na lista do histórico.
-                command_err = True
-            os._exit(0)  # Encerra o processo filho sem mensagem de erro (Tem que encerrar para voltar pro processo pai e aguradar o prox comando)
-            
-    #Aqui o processo pai abaixo é dentificado por pid > 0. Dentro desse if o código aguarda que o processo filho termine usando os.wait(). Após isso, ele faz a verificação dao código de saída do processo filho com a função os.WEXITSTATUS(status). Se o código de saída for 1, então é porque ocorreu um erro ao executar o comando no processo filho. Então eu adicionei um continue para evitar adicionar o comando inválido à lista de comandos utilizados.
+                os.execvp(args[0], args) #nessa linha tenta executar o comando no terminal do linux.
+            except FileNotFoundError: #Caso não tenha sido possível executar, exibe a mensagem avisando o usuário e encerra o processo filho retornando 1.
+                print(f"Comando não encontrado: {args[0]}")
+                os._exit(1) #encerra o processo filho e retorna 1
 
-    elif pid > 0 and command_err == False and command != 'history' and not command.startswith("!"):
-        _, status = os.wait() # "_" é usado para descartar o valor de retorno que os.wait() retorna, que seria o ID do processo que terminou. Nesse caso queremos apenas o satus de saída do processo filho, retornado para a variável status. Nesse caso, ele espera a certificação do status para indicar que o processo filho acabou.
-        command2 = ''
-        if os.WEXITSTATUS(status) == 1 and rodando==True:
-            continue  # Evita adicionar comandos inválidos à lista, pois se caso seja 1 significa que o retorno deu algum tipo de erro.
+        if pid > 0: #Nesse caso voltamos para o processo pai, onde será verificado se o processo filho terminou;
+            _, status = os.wait() #aguarda o processo filho terminar
+            #command2 = ''
+            if os.WEXITSTATUS(status) == 1 and rodando == True: #verifica se o retorno após o processo filho ser finalizado é 1, ou seja, se deu erro, caso sim, utiliza-se o continue para voltar para o início do loop e ignorar a parte abaixo do continue.
+                continue
 
-
-
-    #Comandos especiais. Aqui é um comando do pŕoprio simulador. !! retorna último comando válido, e ! com numeração, retorna um comando específico da lista do histórico.
-    if command == "!!":
-        if len(comandos_utilizados)>0:
-            command2 = comandos_utilizados[len(comandos_utilizados)-1]
-        else:
-            print("Nenhum comando correspondente no histórico.")
-
-    elif command.startswith("!"): #Fiz a verificação do comando '!!' pa ficar mais fácil, aqui consdiera os outros começando por um '!' e um número.
-        try:
-            num = int(command[1:])
-            if num == 1 and comandos_utilizados:
-                command2 = comandos_utilizados[0]
-            elif 1 <= num <= len(comandos_utilizados):
-                command2 = comandos_utilizados[num - 1]
-            else:
-                print("Número de histórico inválido")
-        except ValueError: #tratamento de erro para um erro específico, no caso um valor, ou seja, se for uma posição que não tem no vetor.
-            print("Comando de histórico inválido")
-
-    elif(command_err == False and command != 'history' and not command.startswith("!")): #esse else if é uma condição para adicionar a lista do histórico somente comando válidos, no caso, ele não contabiliza se deu erro no comando, se for o comando history, ou começar por '!', pois são comandos do simulador e não do terminal.
+    if args[0] != "history" and not command.startswith("!"): #caso o comando não tenha sido inválido, o código chega nessa verificaç~ao onde se o comando utilizado nãõ foi o history e nem começa com exclamação, ele é adicionado ao vetor de comandos utilizados.
         comandos_utilizados.append(command)
